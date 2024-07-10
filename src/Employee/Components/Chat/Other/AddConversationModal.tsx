fa22213eefc9f068b5a80b80c364c30408beae00
Andrea Braia <andrix.braia@gmail.com>
2024-07-10 13:17:02 +0200

    Conversation UI Edit

--- src/Employee/Components/Chat/Other/AddConversationModal.tsx
+++ src/Employee/Components/Chat/Other/AddConversationModal.tsx
@@ -14,20 +14,9 @@ import { API_URL_IMG } from "../../../../API/API";
 import axios from "axios";
 
 interface Member {
-  EmployeeId: number;
-  EmployeeFullName: string;
-  EmployeeEmail: string;
-  EmployeeImageUrl: string;
-}
-
-interface ExistingMember {
-  ConversationId: number;
-  Staffer1Id: number;
-  Staffer2Id: number;
-  Staffer1FullName: string;
-  Staffer2FullName: string;
-  lastMessage: string;
-  lastMessageDate?: Date;
+  StafferId: number;
+  StafferFullName: string;
+  StafferImageUrl: string;
 }
 
 export default function AddConversationModal({
@@ -42,45 +31,20 @@ export default function AddConversationModal({
   handleOpenChat: (conversationId: number) => void;
 }) {
   const [member, setMember] = useState<Member>({
-    EmployeeId: -1,
-    EmployeeFullName: "",
-    EmployeeEmail: "",
-    EmployeeImageUrl: "",
+    StafferId: 0,
+    StafferFullName: "",
+    StafferImageUrl: "",
   });
   const [members, setMembers] = useState<Member[]>([]);
 
   useEffect(() => {
     axios
-      .get("/Chat/GET/GetConversationByStafferId", {
+      .get("/Chat/GET/FindStaffersWithoutMessagesFromLoggedStaffer", {
         params: { StafferId: loggedStafferId },
       })
       .then((res) => {
-        let ExistingMembers = res.data as ExistingMember[];
-        axios.get("/Staffer/GET/GetAllStaffers").then((res) => {
-          setMembers(
-            res.data
-              .filter(
-                (staffer: Member) =>
-                  !ExistingMembers.some(
-                    (existingMember) =>
-                      existingMember.Staffer1Id === loggedStafferId &&
-                      existingMember.Staffer2Id === staffer.EmployeeId
-                  ) &&
-                  !ExistingMembers.some(
-                    (existingMember) =>
-                      existingMember.Staffer2Id === loggedStafferId &&
-                      existingMember.Staffer1Id === staffer.EmployeeId
-                  ) &&
-                  staffer.EmployeeId !== loggedStafferId
-              )
-              .map((staffer: Member) => ({
-                EmployeeId: staffer.EmployeeId,
-                EmployeeFullName: staffer.EmployeeFullName,
-                EmployeeEmail: staffer.EmployeeEmail,
-                EmployeeImageUrl: staffer.EmployeeImageUrl,
-              }))
-          );
-        });
+        console.log(res.data);
+        setMembers(res.data);
       });
   }, [loggedStafferId]);
 
@@ -88,7 +52,7 @@ export default function AddConversationModal({
     axios
       .post("/Chat/POST/CreateConversation", {
         Staffer1Id: loggedStafferId,
-        Staffer2Id: member.EmployeeId,
+        Staffer2Id: member.StafferId,
       })
       .then((res) => {
         window.location.reload();
@@ -101,7 +65,7 @@ export default function AddConversationModal({
     <Modal
       isOpen={isOpen}
       onOpenChange={isClosed}
-      size="5xl"
+      size="xl"
       scrollBehavior="inside"
       placement="center"
       backdrop="blur"
@@ -110,57 +74,46 @@ export default function AddConversationModal({
         {() => (
           <>
             <ModalHeader className="flex flex-col gap-1">
-              Aggiunta della conversazione con: {member.EmployeeFullName}
+              Crea conversazione con: {member.StafferFullName}
             </ModalHeader>
             <ModalBody>
-              <div className="mt-6 border-t border-gray-100">
-                <dl className="divide-y divide-gray-100">
-                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium leading-6 text-gray-900">
-                      Dipendente
-                    </dt>
-                    <Autocomplete
-                      defaultItems={members}
-                      placeholder="Cerca per nome..."
-                      className="max-w-xs"
-                      variant="bordered"
-                      radius="sm"
-                      key={members.length}
-                    >
-                      {members.map((member) => (
-                        <AutocompleteItem
-                          startContent={
-                            <Avatar
-                              src={
-                                member.EmployeeImageUrl &&
-                                API_URL_IMG +
-                                  "/profileIcons/" +
-                                  member.EmployeeImageUrl
-                              }
-                              alt={member.EmployeeFullName}
-                            />
-                          }
-                          key={member.EmployeeId}
-                          onClick={() => {
-                            setMember(member);
-                          }}
-                        >
-                          {member.EmployeeFullName}
-                        </AutocompleteItem>
-                      ))}
-                    </Autocomplete>
-                  </div>
-                </dl>
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-6">
+                  <Autocomplete
+                    defaultItems={members}
+                    placeholder="Cerca per nome..."
+                    variant="bordered"
+                    radius="sm"
+                    key={members.length}
+                  >
+                    {members.map((member) => (
+                      <AutocompleteItem
+                        startContent={
+                          <Avatar
+                            src={
+                              member.StafferImageUrl &&
+                              API_URL_IMG +
+                                "/profileIcons/" +
+                                member.StafferImageUrl
+                            }
+                            alt={member.StafferFullName}
+                          />
+                        }
+                        key={member.StafferId}
+                        onClick={() => {
+                          setMember(member);
+                        }}
+                      >
+                        {member.StafferFullName}
+                      </AutocompleteItem>
+                    ))}
+                  </Autocomplete>
+                </div>
               </div>
             </ModalBody>
             <ModalFooter>
-              <Button
-                color="success"
-                variant="light"
-                onClick={createConversation}
-                radius="sm"
-              >
-                Aggiorna
+              <Button color="success" onClick={createConversation} radius="sm">
+                Aggiungi
               </Button>
             </ModalFooter>
           </>
