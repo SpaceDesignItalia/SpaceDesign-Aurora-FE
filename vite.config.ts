e144c889483e671f739f5f8b8d22c2e56bf85f8f
Andrea Braia <andrix.braia@gmail.com>
2024-11-21 15:49:43 +0100

    Fix build prod

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", // Assicurati che l'output venga scritto nella cartella dist
+  },
 });
