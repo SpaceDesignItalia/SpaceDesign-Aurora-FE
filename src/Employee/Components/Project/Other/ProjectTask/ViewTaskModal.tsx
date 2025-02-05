e762f421d8de1b5b0d4ede4689f6599d918fe684
Francesco Roviello <francesco.roviello@spacedesign-italia.it>
2025-02-05 14:43:53 +0100

    Edit icon fix task

--- src/Employee/Components/Project/Other/ProjectTask/ViewTaskModal.tsx
+++ src/Employee/Components/Project/Other/ProjectTask/ViewTaskModal.tsx
@@ -1115,7 +1115,9 @@ export default function ViewTaskModal({
                       </dd>
                     </div>
                     {((newTask!.ProjectTaskDescription &&
-                      hasValidDescription(newTask!.ProjectTaskDescription)) ||
+                      hasValidDescription(
+                        newTask!.ProjectTaskDescription || ""
+                      )) ||
                       editing) && (
                       <div className="px-4 py-6 flex flex-col sm:gap-4 sm:px-0">
                         <dt className="flex flex-row gap-2 items-center text-sm font-semibold leading-6 text-gray-900">
@@ -1140,7 +1142,7 @@ export default function ViewTaskModal({
                                 }
                               />
                               {hasValidDescription(
-                                newTask!.ProjectTaskDescription
+                                newTask!.ProjectTaskDescription || ""
                               ) ? (
                                 <div className="flex justify-center items-center">
                                   <Button
@@ -1165,7 +1167,10 @@ export default function ViewTaskModal({
                                     ) : (
                                       <>
                                         {" "}
-                                        <AutoFixHighRoundedIcon className="w-5 h-5" />{" "}
+                                        <Icon
+                                          icon="solar:magic-stick-3-linear"
+                                          fontSize={24}
+                                        />{" "}
                                         Riscrivi con AI{" "}
                                       </>
                                     )}
