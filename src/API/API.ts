d9145803a7167fe6762fd010a123aa53d8fa4542
Marco Macherelli <elmache05@gmail.com>
2025-02-25 17:10:56 +0100

    ApiFix

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,4 +1,4 @@
-export const API_URL_IMG = "https://api.spacedesign-italia.it";
-export const API_WEBSOCKET_URL = "https://api.spacedesign-italia.it";
-export const API_URL = "https://api.spacedesign-italia.it/API/v1";
+export const API_URL_IMG = "https://api.spacedesign-italia.it:3000";
+export const API_WEBSOCKET_URL = "https://api.spacedesign-italia.it:3000";
+export const API_URL = "https://api.spacedesign-italia.it:3000/API/v1";
 export const FRONTEND_URL = "https://app.spacedesign-italia.it";
