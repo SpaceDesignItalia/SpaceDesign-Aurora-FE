4245a17a75eeabeb0616294636ddc9ae5e605d6a
Marco Macherelli <elmache05@gmail.com>
2025-02-25 17:08:20 +0100

    API for develop

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,4 +1,4 @@
-export const API_URL_IMG = "https://api.spacedesign-italia.it";
-export const API_WEBSOCKET_URL = "https://api.spacedesign-italia.it";
-export const API_URL = "https://api.spacedesign-italia.it/API/v1";
-export const FRONTEND_URL = "https://app.spacedesign-italia.it";
+export const API_URL_IMG = "http://localhost:3000";
+export const API_WEBSOCKET_URL = "http://localhost:3000";
+export const API_URL = "http://localhost:3000/API/v1";
+export const FRONTEND_URL = "http://localhost:5173";
